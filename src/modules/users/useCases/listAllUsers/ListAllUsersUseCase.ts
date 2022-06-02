87e487e0ce792183d5218d19347e597d4d854a78
luciano <luciano.augusto@smarttbotter.com>
2022-06-01 21:31:06 -0300

    ignite

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const users = this.usersRepository.list()
+    const userAdmin = this.usersRepository.findById(user_id)
+
+    if(!userAdmin) {
+      throw new Error('User not Exists!')
+    }
+
+    if(userAdmin.admin === false) {
+      throw new Error('User is not admin')
+    }
+    return users
   }
 }
 
